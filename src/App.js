5e216817a1fb5158bf6ffb2514667e003e62276c
darcrandex <darcrandex@gmail.com>
2019-11-19 18:25:46 +0800

    add router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react'
-
+import { Route, Switch, Redirect } from 'react-router-dom'
 import Lazy from '@/components/Lazy'
 
 const Home = Lazy(() => import('@/pages/Home'))
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <>
+      <Switch>
+        <Route exact path="/" render={props => <Home {...props} />} />
+
+        <Redirect to="/" />
+      </Switch>
+    </>
   )
 }
 
